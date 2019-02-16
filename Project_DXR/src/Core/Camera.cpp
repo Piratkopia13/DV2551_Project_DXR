7f3c875d86da3b30ade2b057e0dd0d13643fe643
Fredrik Junede <ettafredrik@gmail.com>
2019-02-16 21:54:06 +0100

    Implemented c++ class for camera (not implemented in shaders yet)

--- Project_DXR/src/Core/Camera.cpp
+++ Project_DXR/src/Core/Camera.cpp
@@ -2,12 +2,129 @@
 #include "Camera.h"
 
 
+using namespace DirectX;
 
-Camera::Camera()
+Camera::Camera(const float aspectRatio, const float fov, const float nearZ, const float farZ)
+	: m_aspectRatio(aspectRatio)
+	, m_fov(XMConvertToRadians(fov))
+	, m_nearZ(nearZ)
+	, m_farZ(farZ) 
 {
+	m_pos = XMVectorSet(0.f, 0.f, 0.f, 0.f);
+	m_dir = XMVectorSet(0.f, 0.f, -1.f, 0.f);
+	m_up = XMVectorSet(0.f, 1.f, 0.f, 0.f);
+
+	m_VPMatrix = XMMatrixIdentity();
+	m_viewMatNeedsUpdate = true;
+	m_projMatNeedsUpdate = true;
+	m_VPMatNeedsUpdate = true;
 }
 
 
-Camera::~Camera()
+Camera::~Camera() {
+}
+
+void Camera::lookAt(const DirectX::XMVECTOR& position) {
+	m_dir = position - m_pos;
+	m_dir = XMVector3Normalize(m_dir);
+	m_viewMatNeedsUpdate = true;
+	m_VPMatNeedsUpdate = true;
+}
+
+void Camera::setPosition(const DirectX::XMVECTOR& position) {
+	m_pos = position;
+	m_viewMatNeedsUpdate = true;
+	m_VPMatNeedsUpdate = true;
+}
+
+void Camera::move(const DirectX::XMVECTOR& toMove) {
+	m_pos += toMove;
+	m_viewMatNeedsUpdate = true;
+	m_VPMatNeedsUpdate = true;
+}
+
+void Camera::setDirection(const DirectX::XMVECTOR& direction) {
+	m_dir = direction;
+	m_viewMatNeedsUpdate = true;
+	m_VPMatNeedsUpdate = true;
+}
+
+void Camera::setFOV(const float fov) {
+	m_fov = XMConvertToRadians(fov);
+	m_projMatNeedsUpdate = true;
+	m_VPMatNeedsUpdate = true;
+}
+
+void Camera::setAspectRatio(const float aspectRatio) {
+	m_aspectRatio = aspectRatio;
+	m_projMatNeedsUpdate = true;
+	m_VPMatNeedsUpdate = true;
+}
+
+void Camera::setNearZ(const float nearZ) {
+	m_nearZ = nearZ;
+	m_projMatNeedsUpdate = true;
+	m_VPMatNeedsUpdate = true;
+}
+
+void Camera::setFarZ(const float farZ) {
+	m_farZ = farZ;
+	m_projMatNeedsUpdate = true;
+	m_VPMatNeedsUpdate = true;
+}
+
+
+const float Camera::getAspectRatio() const {
+	return m_aspectRatio;
+}
+
+const float Camera::getFOV() const {
+	return m_fov;
+}
+
+const float Camera::getNearZ() const {
+	return m_nearZ;
+}
+
+const float Camera::getFarZ() const {
+	return m_farZ;
+}
+
+const DirectX::XMFLOAT3 Camera::getPosition() {
+	XMFLOAT3 toReturn;
+	XMStoreFloat3(&toReturn, m_pos);
+	return toReturn;
+}
+
+const DirectX::XMFLOAT3 Camera::getDirection() {
+	XMFLOAT3 toReturn;
+	XMStoreFloat3(&toReturn, m_dir);
+	return toReturn;
+}
+
+const DirectX::XMMATRIX Camera::getViewMatrix()
 {
+	if (m_viewMatNeedsUpdate) {
+		m_viewMatrix = XMMatrixLookToLH(m_pos, m_dir, m_up);
+	}
+
+	return m_viewMatrix;
+}
+
+const DirectX::XMMATRIX Camera::getProjMatrix()
+{
+	if (m_projMatNeedsUpdate) {
+		m_projMatrix = XMMatrixPerspectiveFovLH(m_fov, m_aspectRatio, m_nearZ, m_farZ);
+	}
+
+	return m_projMatrix;
+}
+
+const DirectX::XMMATRIX Camera::getVPMatrix() {
+
+	if (m_VPMatNeedsUpdate) {
+		m_VPMatrix = getViewMatrix() * getProjMatrix();
+	}
+
+	return m_VPMatrix;
 }
--- Project_DXR/src/Core/Camera.h
+++ Project_DXR/src/Core/Camera.h
@@ -1,8 +1,54 @@
 #pragma once
+
+#include <DirectXMath.h>
+
 class Camera
 {
 public:
-	Camera();
+	Camera(const float aspectRatio, const float fov, const float nearZ, const float farZ);
 	~Camera();
+
+	void lookAt(const DirectX::XMVECTOR& position);
+
+	void move(const DirectX::XMVECTOR& toMove);
+
+	void setPosition(const DirectX::XMVECTOR& position);
+	void setDirection(const DirectX::XMVECTOR& direction);
+	void setFOV(const float fov);
+	void setAspectRatio(const float aspectRatio);
+	void setNearZ(const float nearZ);
+	void setFarZ(const float farZ);
+
+	const float getAspectRatio() const;
+	const float getFOV() const;
+	const float getNearZ() const;
+	const float getFarZ() const;
+
+	const DirectX::XMFLOAT3 getPosition();
+	//const DirectX::XMFLOAT3 getLookAt();
+	const DirectX::XMFLOAT3 getDirection();
+
+	const DirectX::XMMATRIX getViewMatrix();
+	const DirectX::XMMATRIX getProjMatrix();
+	const DirectX::XMMATRIX getVPMatrix();
+
+
+private:
+	DirectX::XMMATRIX m_viewMatrix;
+	DirectX::XMMATRIX m_projMatrix;
+	DirectX::XMMATRIX m_VPMatrix;
+
+	bool m_projMatNeedsUpdate;
+	bool m_viewMatNeedsUpdate;
+	bool m_VPMatNeedsUpdate;
+
+	DirectX::XMVECTOR m_pos;
+	DirectX::XMVECTOR m_dir;
+	DirectX::XMVECTOR m_up;
+
+	float m_nearZ;
+	float m_farZ;
+	float m_aspectRatio;
+	float m_fov;
 };
 
