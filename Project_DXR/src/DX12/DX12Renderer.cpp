7b51be3dab5429584e0b6b17d1b4d930b8a1818e
Fredrik Junede <ettafredrik@gmail.com>
2019-02-19 16:08:55 +0100

    Fixed ImGui rendering

--- Project_DXR/src/DX12/DX12Renderer.cpp
+++ Project_DXR/src/DX12/DX12Renderer.cpp
@@ -478,6 +478,8 @@ HRESULT DX12Renderer::initImGui() {
 		DXGI_FORMAT_R8G8B8A8_UNORM,
 		m_ImGuiSrvDescHeap->GetCPUDescriptorHandleForHeapStart(),
 		m_ImGuiSrvDescHeap->GetGPUDescriptorHandleForHeapStart());
+
+	return S_OK;
 }
 
 void DX12Renderer::createShaderResources() {
@@ -669,6 +671,9 @@ void DX12Renderer::frame() {
 	} else {
 		// ImGui
 		{
+			m_postCommand.list->OMSetRenderTargets(1, &m_cdh, true, nullptr);
+			m_postCommand.list->SetGraphicsRootSignature(m_globalRootSignature.Get());
+
 			// Start the Dear ImGui frame
 			ImGui_ImplDX12_NewFrame();
 			ImGui_ImplWin32_NewFrame();
@@ -683,38 +688,6 @@ void DX12Renderer::frame() {
 			// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
 			if (show_demo_window)
 				ImGui::ShowDemoWindow(&show_demo_window);
-
-			// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-			{
-				static float f = 0.0f;
-				static int counter = 0;
-
-				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-				ImGui::Checkbox("Another Window", &show_another_window);
-
-				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-					counter++;
-				ImGui::SameLine();
-				ImGui::Text("counter = %d", counter);
-
-				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-				ImGui::End();
-			}
-
-			// 3. Show another simple window.
-			if (show_another_window) {
-				ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-				ImGui::Text("Hello from another window!");
-				if (ImGui::Button("Close Me"))
-					show_another_window = false;
-				ImGui::End();
-			}
 
 			// Set the descriptor heaps
 			ID3D12DescriptorHeap* descriptorHeaps[] = { m_ImGuiSrvDescHeap.Get() };
